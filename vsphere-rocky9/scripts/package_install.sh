d8cdcf46dcab7338c2f26e6d9cb3738a06119266
Edward Ingram <edward.ingram23@gmail.com>
2023-04-28 14:55:22 -0700

    remove cloud-init

--- vsphere-rocky9/scripts/package_install.sh
+++ vsphere-rocky9/scripts/package_install.sh
@@ -1,5 +1,5 @@
 dnf update -y
 firewall-cmd --permanent --zone=public --add-port=9090/tcp
-dnf install cloud-init perl wget curl vim nano net-tools -y
+dnf install perl wget curl vim nano net-tools -y
 vmware-toolbox-cmd config set deployPkg enable-customization true
 vmware-toolbox-cmd config set deployPkg enable-custom-scripts true
